1e743199df59013e875d9225ca4d0135894d8783
mohmedsabry <mayjak60@gmail.com>
2023-07-26 17:03:40 +0300

    fix buges

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,25 +1,32 @@
 fun main() {
     println("Enter number of student:")
     var n:Int = readLine()!!.toInt()
-    var listOfStudent:ArrayList<Student> = ArrayList(n)
-    for(i in 1 .. n){
+    println("enter num of subject:")
+    var numOfSubject = readLine()!!.toInt()
+    var listOfStudent:MutableList<Student> = MutableList(n){
+        Student("ahmed", MutableList<Subject>(n){
+            Subject("os",50.5)
+        })
+    }
+    for(i in 0 until  n){
         println(" enter the student name:")
         listOfStudent[i].name = readLine()!!.toString()
-        println("enter num of subject:")
-        var numOfStudent = readLine()!!.toInt()
-        for(i in 1..numOfStudent){
+        for(j in 0 until numOfSubject){
             println("enter subject name:")
             var subjectName = readLine()!!.toString()
             println("Enter subject grade:")
             var grade = readLine()!!.toDouble()
-            listOfStudent[i].list.add(Subject(subjectName,grade))
+            listOfStudent[j].list.add(Subject(subjectName,grade))
         }
+        listOfStudent[i].calcGPA()
+        listOfStudent[i].calacTotal()
     }
+
     for (i in listOfStudent){
         println("${i.name} ${i.total} ${i.GPA}")
     }
     listOfStudent.sortBy {
-        it.GPA
+        it.total
     }
     for (i in listOfStudent){
         println("${i.name} ${i.total} ${i.GPA}")
